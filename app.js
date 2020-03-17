137cbf9bc06d8e7bbbd4ac5bc71d83e9e630e701
Tumwesigye <Tumwesigye>
2020-03-17 13:46:24 +0300

    Added a views folder, static files and tracked these using express

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
-var express = require("express")
-var chalk = require("chalk")
+var express = require("express");
+var chalk = require("chalk");
+// var debug = require("debug")(app)
+var morgan = require("morgan");
+var path = require("path");
 
-var app = express()
+var app = express();
 
-app.get("/", function(req, res){
-    res.send('Hello from LibraryJs')
-})
+app.use(morgan("combined"));
+app.use(express.static(path.join(__dirname, "/public/")))
+app.use("/css", express.static(path.join(__dirname, "/node_modules/bootstrap/dist/css")))
+app.use("/js", express.static(path.join(__dirname, "/node_modules/bootstrap/dist/js")))
+app.use("/js", express.static(path.join(__dirname, "/node_modules/jquery/dist")))
 
+app.get("/", function(req, res) {
+  // res.send('Hello from my Library app')
+  res.sendFile(path.join(__dirname, "/views/index.html"));
+});
 
-
-app.listen(3000, function(){
-    console.log("Listening on port 3000")
-})
+app.listen(3000, function() {
+  // debug(`Listening on port ${chalk.green(3000)}`)
+  console.log(`Listening on port ${chalk.green(3000)}`);
+});
