0e76ee8bee55819d717581224a75387f4f332a80
Tumwesigye <Tumwesigye>
2020-04-09 21:14:42 +0300

    Refactored code

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
+// Require modules
 const express = require('express');
 const chalk = require('chalk');
 // const debug = require('debug')('app')
 const morgan = require('morgan');
 const path = require('path');
 
+// windows linebreaks when not in production environment
+"rules"; {
+  "linebreak-style"; ["error", process.env.NODE_ENV === 'prod' ? "unix" : "windows"]
 
 const app = express();
-const port = process.env.PORT;
-app.use(morgan('combined'));
 
+// Set port 
+const port = process.env.PORT;
+
+//Register middleware
+app.use(morgan('combined'));
 app.use(express.static(path.join(__dirname, '/public/')));
 app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
 app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')));
--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -2,58 +2,69 @@ const express = require('express');
 
 const bookRouter = express.Router();
 
-function router(nav){
+// windows linebreaks when not in production environment
+"rules"; {
+  "linebreak-style"; ["error", process.env.NODE_ENV === 'prod' ? "unix" : "windows"]
+
+// Books collection array
+function router(nav) {
   let books = [
     {
-      title:'A journey into the center of the earth',
-      genre:'Science Fiction',
-      author:'Jule Verne',
-      read: false
+      title: 'A journey into the center of the earth',
+      genre: 'Science Fiction',
+      author: 'Jule Verne',
+      read: false,
     },
     {
-      title:'The dark world',
-      genre:'Fantasy',
-      author:'Henry Kuttner',
-      read: false
+      title: 'The dark world',
+      genre: 'Fantasy',
+      author: 'Henry Kuttner',
+      read: false,
     },
     {
-      title:'The wind in the willows',
-      genre:'Fantasy',
-      author:'keneth Grahame',
-      read: false
+      title: 'The wind in the willows',
+      genre: 'Fantasy',
+      author: 'keneth Grahame',
+      read: false,
     },
     {
-      title:'War and peace',
-      genre:'Historical Fiction',
-      author:'Lev Nikolayevich Tolstoy',
-      read: false
+      title: 'War and peace',
+      genre: 'Historical Fiction',
+      author: 'Lev Nikolayevich Tolstoy',
+      read: false,
     },
     {
-      title:'Les Miserable',
-      genre:'Historical Fiction',
-      author:'Victor Hugo',
-      read: false
-    }
-  ]
-  
-  bookRouter.route('/').get((req, res)=>{
+      title: 'Les Miserable',
+      genre: 'Historical Fiction',
+      author: 'Victor Hugo',
+      read: false,
+    },
+  ];
+
+  // Display book catalogue
+  bookRouter.route('/').get((req, res) => {
     res.render('bookList', {
       nav,
       title: 'Book repository',
-      books
+      books,
     });
   });
-  
-  bookRouter.route('/:id').get((req, res)=>{
-    const {id} = req.params
-  
+
+  // Specific book route
+  bookRouter.route('/:id').get((req, res) => {
+    const { id } = req.params;
+
     res.render('bookView', {
-      nav: [{ link: '/books', title: 'Books' }, { link: '/authors', title: 'Authors' }],
+      nav: [
+        { link: '/books', title: 'Books' },
+        { link: '/authors', title: 'Authors' },
+      ],
       title: 'Book Details',
-      book: books[id]
+      book: books[id],
     });
   });
-  return bookRouter
+  return bookRouter;
 }
 
-module.exports = router
+//Exports
+module.exports = router;
