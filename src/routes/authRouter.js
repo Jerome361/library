241dc3ffe01fd3d26ae7bf7fce20e9492a22266b
Tumwesigye <Tumwesigye>
2020-04-12 13:20:51 +0300

    Enabled user login

--- src/routes/authRouter.js
+++ src/routes/authRouter.js
@@ -1,15 +1,27 @@
 const express = require('express');
-const { MongoClient } = require('mongodb')
+const { MongoClient } = require('mongodb');
 
-const authRouter = express.Router()
+const authRouter = express.Router();
 
 function router() {
   authRouter.route('/signUp')
     .post((req, res) => {
-      console.log(req.body)
-      res.json(req.body)
-    })
-  return authRouter
+      console.log(req.body);
+      // Create user, log them in and take them to a page;
+      req.login(req.body, () => {
+        res.redirect('/auth/profile');
+      });
+    });
+
+  // Because I have logged in, passport will take the user, serialise it into the cookie and all
+  // that magic for us using local strategy, but when a request is made to /profile,
+  // it will attach the user as if by magic to the request;
+
+  authRouter.route('/profile')
+    .get((req, res) => {
+      res.json(req.user);
+    });
+  return authRouter;
 }
 
-module.exports = router
+module.exports = router;
