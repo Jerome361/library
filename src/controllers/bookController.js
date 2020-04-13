86de0504e16495ed27d27471218d825c57d6e59d
Tumwesigye <Tumwesigye>
2020-04-13 23:12:42 +0300

    Implemented controllers

---
+++ src/controllers/bookController.js
@@ -0,0 +1,92 @@
+// // windows linebreaks when not in production environment
+// "rules" = {
+//   "linebreak-style" : ["error", process.env.NODE_ENV === 'prod' ? "unix" : "windows"]
+// }
+const { MongoClient, ObjectId } = require('mongodb');
+
+function bookController(nav) {
+    // Authorisation
+    function middleware(req, res, next) {
+        if (req.user) next();
+        else res.redirect('/');
+      }
+      // Index route
+  function getIndex(req, res) {
+    const url = 'mongodb://localhost:27017/myLibraryApp';
+
+    (async function mongo() {
+      let client;
+      try {
+        // Connecting to the server
+        client = await MongoClient.connect(url);
+        console.log('Server connection successful');
+
+        // Create a new database instance
+        const db = client.db();
+
+        // Create a collection instance and insert books to it.
+        const col = await db.collection('books');
+
+        // fetch books;
+        const books = await col.find().toArray();
+        await console.log(books);
+
+        res.render('bookList', {
+          nav,
+          title: 'Book repository',
+          books,
+        });
+      } catch (err) {
+        console.err(`Database error ${err}`);
+      }
+
+      client.close();
+    }());
+  }
+  // Question ID route
+  function getById(req, res) {
+    const { id } = req.params;
+
+    const url = 'mongodb://localhost:27017/myLibraryApp';
+
+    (async function mongo() {
+      let client;
+      try {
+        // Connecting to the server
+        client = await MongoClient.connect(url);
+        console.log('Server connection successful');
+
+        // Create a new database instance
+        const db = client.db();
+
+        // Create a collection instance and insert books to it.
+        const col = await db.collection('books');
+
+        // fetch specific books
+        const book = await col.findOne({ _id: ObjectId(id) });
+        await console.log(book);
+
+        res.render('bookView', {
+          nav: [
+            { link: '/books', title: 'Books' },
+            { link: '/authors', title: 'Authors' },
+          ],
+          title: 'Book Details',
+          book,
+        });
+      } catch (err) {
+        console.err(`Database error ${err}`);
+      }
+
+      client.close();
+    }());
+  }
+
+  return {
+    middleware,
+    getIndex,
+    getById,
+  };
+}
+
+module.exports = bookController;
--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -2,85 +2,19 @@ const express = require('express');
 
 const bookRouter = express.Router();
 const { MongoClient, ObjectId } = require('mongodb');
+const bookController = require('../controllers/bookController');
 
 function router(nav) {
+  const { getIndex, getById, middleware } = bookController(nav);
   // authorising access to only signed in users;
-  bookRouter.use((req, res, next) => {
-    if (req.user) next();
-    else res.redirect('/');
-  });
+  bookRouter.use(middleware);
   // Display book catalogue;
-  bookRouter.route('/').get((req, res) => {
-    const url = 'mongodb://localhost:27017/myLibraryApp';
+  bookRouter.route('/')
+    .get(getIndex);
 
-    (async function mongo() {
-      let client;
-      try {
-        // Connecting to the server
-        client = await MongoClient.connect(url);
-        console.log('Server connection successful');
-
-        // Create a new database instance
-        const db = client.db();
-
-        // Create a collection instance and insert books to it.
-        const col = await db.collection('books');
-
-        // fetch books
-        const books = await col.find().toArray();
-        await console.log(books);
-
-        res.render('bookList', {
-          nav,
-          title: 'Book repository',
-          books
-        });
-      } catch (err) {
-        console.err(`Database error ${err}`);
-      }
-
-      client.close();
-    }());
-  });
-
-  // Specific book route
-  bookRouter.route('/:id').get((req, res) => {
-    const { id } = req.params;
-
-    const url = 'mongodb://localhost:27017/myLibraryApp';
-
-    (async function mongo() {
-      let client;
-      try {
-        // Connecting to the server
-        client = await MongoClient.connect(url);
-        console.log('Server connection successful');
-
-        // Create a new database instance
-        const db = client.db();
-
-        // Create a collection instance and insert books to it.
-        const col = await db.collection('books');
-
-        // fetch specific books
-        const book = await col.findOne({ _id: ObjectId(id) });
-        await console.log(book);
-
-        res.render('bookView', {
-          nav: [
-            { link: '/books', title: 'Books' },
-            { link: '/authors', title: 'Authors' },
-          ],
-          title: 'Book Details',
-          book
-        });
-      } catch (err) {
-        console.err(`Database error ${err}`);
-      }
-
-      client.close();
-    }());
-  });
+  // Specific book route;
+  bookRouter.route('/:id')
+    .get(getById);
   return bookRouter;
 }
 
