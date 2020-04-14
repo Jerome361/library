9a504d5f7bdccf0d85fcdaa57650f863eacd91d4
Tumwesigye <Tumwesigye>
2020-04-14 19:36:17 +0300

    Implemented a Goodreads API call

--- src/services/bookService.js
+++ src/services/bookService.js
@@ -1,8 +1,26 @@
+const axios = require('axios');
+const xml2js = require('xml2js');
+
+const parser = xml2js.Parser({ explicitArray: false });
 
 function goodReadsService() {
   function getBookById() {
     return new Promise((resolve, reject) => {
-      resolve({ description: 'Our description' });
+      axios.get('https://www.goodreads.com/book/show/656.xml?key=AVJNvCywwX9z4rmh1Y9bQ')
+        .then((response) => {
+          parser.parseString(response.data, (err, result) => {
+            if (err) console.log(err);
+            else {
+              console.log(result);
+              resolve(result.GoodreadsResponse.book);
+            //   resolve({ description: 'Our Description' })
+            }
+          });
+        })
+        .catch((error) => {
+          reject(error);
+          console.log(error);
+        });
     });
   }
   return { getBookById };
